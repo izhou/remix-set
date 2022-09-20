6dc44383e06b021709076c6584d89fdc70acb611
Irene <i@ire.ne>
2022-09-20 16:18:27 -0700

    fix netlify fn

--- netlify/functions/create-daily-puzzle.ts
+++ netlify/functions/create-daily-puzzle.ts
@@ -1,25 +1,24 @@
-import { schedule } from '@netlify/functions';
-import { choosePuzzleCards } from '~/utils/cards';
-import { db } from '~/utils/db.server';
-
+import { schedule } from "@netlify/functions";
+import { choosePuzzleCards } from "~/utils/game";
+import { db } from "~/utils/db.server";
 
 const createDailyPuzzle = async () => {
   let cards = JSON.stringify(choosePuzzleCards());
-  // Return in yyyy-mm-dd 
+  // Return in yyyy-mm-dd
   let date = new Date();
-  let formattedDate = date.toISOString().slice(0,10);
+  let formattedDate = date.toISOString().slice(0, 10);
 
   await db.dailyPuzzle.create({
     data: {
-      cards, 
-      date: formattedDate
+      cards,
+      date: formattedDate,
     },
   });
 
   return {
     statusCode: 200,
-    body:  `Puzzle for ${date.toISOString()} created.`,
+    body: `Puzzle for ${date.toISOString()} created.`,
   };
 };
 
-export const handler = schedule('0 7 * * *', createDailyPuzzle);
+export const handler = schedule("0 7 * * *", createDailyPuzzle);
