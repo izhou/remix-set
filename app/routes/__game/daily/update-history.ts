a276a73bd4a18b016fc37bac5a25acb4842f8ae5
Irene <i@ire.ne>
2022-09-27 14:19:50 -0700

    clean up update-history

--- app/routes/__game/daily/update-history.ts
+++ app/routes/__game/daily/update-history.ts
@@ -24,14 +24,6 @@ export const action: ActionFunction = async ({ request }) => {
   const session = await getSession(request.headers.get("Cookie"));
   let sessionHistory = session.get("history") || {};
 
-  if (sessionHistory[puzzleDate]) {
-    setIndexes = [
-      ...new Set(
-        setIndexes.concat(sessionHistory[puzzleDate] as Array<SetIndex>)
-      ),
-    ];
-  }
-
   if (!userId) {
     // If not logged in, store history in the session
     sessionHistory[puzzleDate] = setIndexes;
@@ -60,59 +52,3 @@ export const action: ActionFunction = async ({ request }) => {
     { headers: { "Set-Cookie": await commitSession(session) } }
   );
 };
-
-// export const action: ActionFunction = async ({ request }) => {
-//   const form = await request.formData();
-
-//   let puzzleDate = form.get("date");
-//   let jsonSets = form.get("foundSets");
-
-//   if (typeof puzzleDate !== "string" || typeof jsonSets !== "string") {
-//     return json(form, { status: 400 });
-//   }
-
-//   let foundSets: Array<SetIndex> = JSON.parse(jsonSets);
-//   let stringifiedSets = foundSets.map((index) => JSON.stringify(index));
-
-//   const userId = await getUserId(request);
-
-//   // Check for saved history in thesession.
-//   const session = await getSession(request.headers.get("Cookie"));
-//   let sessionHistory = session.get("history") || {};
-
-//   if (sessionHistory[puzzleDate]) {
-//     stringifiedSets = [
-//       ...new Set(stringifiedSets.concat(sessionHistory[puzzleDate])),
-//     ];
-//   }
-
-//   if (!userId) {
-//     // If not logged in, store history in the session
-//     sessionHistory[puzzleDate] = stringifiedSets;
-//   } else {
-//     // If logged in, clear the session and store history in the db instead.
-//     delete sessionHistory[puzzleDate];
-//     const puzzleHistory = await db.dailyPuzzleHistory.upsert({
-//       where: { userId_puzzleDate: { puzzleDate, userId } },
-//       update: { foundSets: stringifiedSets },
-//       create: {
-//         userId,
-//         puzzleDate,
-//         foundSets: stringifiedSets,
-//       },
-//     });
-
-//     stringifiedSets = puzzleHistory.foundSets;
-//   }
-
-//   session.set("history", sessionHistory);
-
-//   return json(
-//     { foundSets: stringifiedSets },
-//     {
-//       headers: {
-//         "Set-Cookie": await commitSession(session),
-//       },
-//     }
-//   );
-// };
